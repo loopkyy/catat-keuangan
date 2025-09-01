a2819c03e0b2a6e2ba95f66f02164172ebaa0f5c
Rizky riaadha rismandana <riaadha.rizky@gmail.com>
2025-09-01 17:48:43 +0700

    Add transactions migration with foreign keys

---
+++ database/migrations/2025_09_01_104831_create_transactions_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+       Schema::create('transactions', function (Blueprint $table) {
+    $table->id();
+    $table->string('title');
+    $table->text('description')->nullable();
+    $table->enum('type', ['income', 'expense']);
+    $table->decimal('amount', 15, 2);
+    $table->date('date');
+    $table->foreignId('category_id')->nullable()->constrained()->nullOnDelete();
+    $table->foreignId('source_id')->nullable()->constrained()->nullOnDelete();
+    $table->foreignId('goal_id')->nullable()->constrained()->nullOnDelete();
+    $table->timestamps();
+});
+
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('transactions');
+    }
+};
